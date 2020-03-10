77449f1a25148f29d60d87ad74ad7e9c79eb18a1
Jim Kitchen <jim22k@gmail.com>
2020-03-10 10:41:22 -0500

    Auto-init
    No longer require users to explicitly call `grblas.init()`.
    First usage of items within grblas will trigger the auto-init.
    Attempting to call init() after the auto-init will raise an error.
    
    Remove dependency on ss_graphblas; switch to conda-forge's graphblas

--- grblas/__init__.py
+++ grblas/__init__.py
@@ -1,27 +1,58 @@
 import importlib
 from . import backends
 
-lib = None
-ffi = None
-REPLACE = None
-Matrix = None
-Vector = None
-Scalar = None
-UnaryOp, BinaryOp, Monoid, Semiring = None, None, None, None
+_init_params = None
+_SPECIAL_ATTRS = ["lib", "ffi", "Matrix", "Vector", "Scalar", "UnaryOp", "BinaryOp", "Monoid", "Semiring"]
 
-def init(backend='suitesparse', blocking=True):
-    global lib, ffi, REPLACE, Matrix, Vector, Scalar, UnaryOp, BinaryOp, Monoid, Semiring
+
+def __getattr__(name):
+    """Auto-initialize if special attrs used without explicit init call by user"""
+    if name in _SPECIAL_ATTRS:
+        _init("suitesparse", True, automatic=True)
+
+        return globals()[name]
+    else:
+        raise AttributeError(f"module {__name__!r} has not attribute {name!r}")
+
+
+def __dir__():
+    attrs = list(globals())
+    if "lib" not in attrs:
+        attrs += _SPECIAL_ATTRS
+    return attrs
+
+
+def init(backend="suitesparse", blocking=True):
+    _init(backend, blocking)
+
+
+def _init(backend, blocking, automatic=False):
+    global lib, ffi, REPLACE, Matrix, Vector, Scalar, UnaryOp, BinaryOp, Monoid, Semiring, _init_params
+
+    passed_params = dict(backend=backend, blocking=blocking, automatic=automatic)
+    if _init_params is None:
+        _init_params = passed_params
+    else:
+        if _init_params != passed_params:
+            from .exceptions import GrblasException
+            if _init_params.get("automatic"):
+                raise GrblasException("grblas objects accessed prior to manual initialization")
+            else:
+                raise GrblasException("grblas initialized multiple times with different init parameters")
+
+        # Already initialized with these parameters; nothing more to do
+        return
 
     ffi_backend = importlib.import_module(f'.backends.{backend}', __name__)
 
-    lib = ffi_backend.lib
-    ffi = ffi_backend.ffi
+    globals()["lib"] = ffi_backend.lib
+    globals()["ffi"] = ffi_backend.ffi
 
     # This must be called before anything else happens
     if blocking:
-        lib.GrB_init(lib.GrB_BLOCKING)
+        ffi_backend.lib.GrB_init(ffi_backend.lib.GrB_BLOCKING)
     else:
-        lib.GrB_init(lib.GrB_NONBLOCKING)
+        ffi_backend.lib.GrB_init(ffi_backend.lib.GrB_NONBLOCKING)
 
     from .base import REPLACE
     from .matrix import Matrix
@@ -33,3 +64,12 @@ def init(backend='suitesparse', blocking=True):
     BinaryOp._initialize()
     Monoid._initialize()
     Semiring._initialize()
+
+    globals()["Matrix"] = Matrix
+    globals()["Vector"] = Vector
+    globals()["Scalar"] = Scalar
+    globals()["UnaryOp"] = UnaryOp
+    globals()["BinaryOp"] = BinaryOp
+    globals()["Monoid"] = Monoid
+    globals()["Semiring"] = Semiring
+    globals()["REPLACE"] = REPLACE
