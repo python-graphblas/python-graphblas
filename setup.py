6799d3cee3c77e0b22f10e2e69ebb9a3fd415f80
Jim Kitchen <jim22k@gmail.com>
2020-04-23 11:00:16 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name='grblas',
-    version='1.2.4',
+    version='1.2.6',
     description='Python interface to GraphBLAS',
     author='Jim Kitchen',
-    packages=['grblas', 'grblas/backends', 'grblas/backends/suitesparse'],
+    packages=['grblas', 'grblas/backends', 'grblas/backends/suitesparse',
+              'grblas/binary', 'grblas/monoid', 'grblas/semiring', 'grblas/unary'],
     setup_requires=["cffi>=1.0.0", "pytest-runner"],
     cffi_modules=["grblas/backends/suitesparse/build.py:ffibuilder"],
     install_requires=["cffi>=1.0.0"],
