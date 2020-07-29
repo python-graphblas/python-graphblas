d3ec1f89fe5c415488276342898959a9b7ab6c69
Erik Welch <erik.n.welch@gmail.com>
2020-07-29 12:14:49 -0500

    Allow to test in non-blocking mode (and add to TravisCI matrix)

--- test/conftest.py
+++ test/conftest.py
@@ -6,13 +6,16 @@ def pytest_addoption(parser):
         "--backend", action="store", default="suitesparse", help="name of a backend in grblas.backends"
     )
     parser.addoption("--runslow", action="store_true", help="run slow tests")
+    parser.addoption("--blocking", dest='blocking', default=True, action="store_true", help="run in blocking mode")
+    parser.addoption("--nonblocking", "--no-blocking", "--non-blocking", dest='blocking', action="store_false", help="run in non-blocking mode")
 
 
 def pytest_configure(config):
     backend = config.getoption('--backend')
+    blocking = config.getoption('--blocking')
     import grblas
-    grblas.init(backend)
-    print(f'Running tests with "{backend}" backend')
+    grblas.init(backend, blocking=blocking)
+    print(f'Running tests with "{backend}" backend, blocking={blocking}')
 
 
 def pytest_runtest_setup(item):
