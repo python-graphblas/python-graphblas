04165886b530082d1f99c0ffb2224b6224db88bc
Erik Welch <erik.n.welch@gmail.com>
2020-07-30 18:18:57 -0500

    Enable branch coverage; test and cover auto-init

---
+++ test/auto_init.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    import pytest
+    import grblas
+    grblas.Matrix
+    with pytest.raises(grblas.exceptions.GrblasException, match='grblas objects accessed prior to manual initialization'):
+        grblas.init()
