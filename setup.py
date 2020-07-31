08d0def398176580ae348f303888458c6555c399
Erik Welch <erik.n.welch@gmail.com>
2020-07-31 16:04:38 -0500

    Add long description and classifiers to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, find_packages
 import versioneer
 
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name='grblas',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='Python interface to GraphBLAS',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jim Kitchen and Erik Welch',
     url='https://github.com/metagraph-dev/grblas',
     packages=find_packages(exclude=['grblas.backends.python']),
@@ -14,4 +19,18 @@ setup(
     cffi_modules=["grblas/backends/suitesparse/build.py:ffibuilder"],
     install_requires=["cffi>=1.0.0"],
     tests_require=["pytest", "pandas"],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
 )
