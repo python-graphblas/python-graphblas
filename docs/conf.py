92b8f5eb47a997df7dfdbfaf6c01bffc6f1da5da
Stan Seibert <sseibert@anaconda.com>
2022-03-16 15:43:21 -0500

    black reformat the entire conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'grblas'
-copyright = '2022, Anaconda, Inc'
-author = 'Anaconda, Inc'
+project = "grblas"
+copyright = "2022, Anaconda, Inc"
+author = "Anaconda, Inc"
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.2'
+release = "1.3.2"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,16 +30,15 @@ release = '1.3.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -47,9 +46,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
