a8b35fb55a6723234483e84ef2d225c432eb5d93
Erik Welch <erik.n.welch@gmail.com>
2022-05-22 14:15:41 -0500

    Test merging matrix masks too

--- graphblas/tests/test_mask.py
+++ graphblas/tests/test_mask.py
@@ -1,11 +1,12 @@
 import itertools
 
-from pytest import raises
+import pytest
 
 from graphblas import Vector
 
 
-def test_mask_new():
+@pytest.mark.parametrize("as_matrix", [False, True])
+def test_mask_new(as_matrix):
     for dtype, mask_dtype in itertools.product([None, bool, int], [bool, int]):
         v1 = Vector(mask_dtype, size=10)
         v1[3:6] = 0
@@ -13,11 +14,16 @@ def test_mask_new():
         v2 = Vector(mask_dtype, size=10)
         v2[1::3] = 0
         v2[::3] = 10
+        if as_matrix:
+            v1 = v1._as_matrix()
+            v2 = v2._as_matrix()
         name = "howdy"
         masks = [v1.S, v1.V, ~v1.S, ~v1.V, v2.S, v2.V, ~v2.S, ~v2.V]
         for m1, m2 in itertools.product(masks, masks):
-            expected = Vector(bool if dtype is None else dtype, size=v1.size)
-            expected << True
+            expected = Vector(bool if dtype is None else dtype, size=10)
+            if as_matrix:
+                expected = expected._as_matrix()
+            expected[...] << True
             expected = expected.dup(mask=m1).dup(mask=m2)
             result = m1.new(dtype, mask=m2, name=name)
             assert result.name == name
@@ -30,7 +36,7 @@ def test_mask_new():
         # w/o second mask
         for m in masks:
             expected.clear()
-            expected << True
+            expected[...] << True
             expected = expected.dup(mask=m)
             result = m.new(dtype, name=name)
             assert result.name == name
@@ -40,7 +46,7 @@ def test_mask_new():
             result = m.new(dtype, complement=True, name=name)
             assert result.name == name
             assert result.isequal(expected, check_dtype=True)
-        with raises(TypeError, match="Invalid mask"):
+        with pytest.raises(TypeError, match="Invalid mask"):
             m.new(mask=object())
-        with raises(TypeError, match="Mask must indicate"):
+        with pytest.raises(TypeError, match="Mask must indicate"):
             m.new(mask=v1)
