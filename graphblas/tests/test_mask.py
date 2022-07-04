995c02266c772fc87be7bf35aded8dbc914eac2d
Erik Welch <erik.n.welch@gmail.com>
2022-07-04 14:12:18 -0500

    Allow masks to be combined with `|` and`&`
    Closes #234

--- graphblas/tests/test_mask.py
+++ graphblas/tests/test_mask.py
@@ -3,6 +3,7 @@ import itertools
 import pytest
 
 from graphblas import Vector
+from graphblas.mask import Mask
 
 
 @pytest.mark.parametrize("as_matrix", [False, True])
@@ -53,3 +54,73 @@ def test_mask_new(as_matrix):
         else:
             with pytest.raises(TypeError, match="Mask must be"):
                 m.new(mask=v1)
+
+
+@pytest.mark.parametrize("as_matrix", [False, True])
+def test_mask_or(as_matrix):
+    for mask_dtype in [bool, int]:
+        v1 = Vector(mask_dtype, size=10)
+        v1[3:6] = 0
+        v1[:3] = 10
+        v2 = Vector(mask_dtype, size=10)
+        v2[1::3] = 0
+        v2[::3] = 10
+        if as_matrix:
+            v1 = v1._as_matrix()
+            v2 = v2._as_matrix()
+        masks = [v1.S, v1.V, ~v1.S, ~v1.V, v2.S, v2.V, ~v2.S, ~v2.V]
+        for m1, m2 in itertools.product(masks, masks):
+            expected = Vector(bool, size=10)
+            if as_matrix:
+                expected = expected._as_matrix()
+            expected(m1) << True
+            expected(m2) << True
+            result = (m1 | m2).new()
+            assert result.isequal(expected, check_dtype=True)
+        with pytest.raises(TypeError, match="Invalid mask"):
+            m1 | object()
+        with pytest.raises(TypeError, match="Invalid mask"):
+            object() | m1
+        if v1.dtype == bool:
+            assert isinstance(m1 | v1, Mask)
+            assert isinstance(v1 | m1, Mask)
+        else:
+            with pytest.raises(TypeError, match="Mask must be"):
+                m1 | v1
+            with pytest.raises(TypeError, match="Mask must be"):
+                v1 | m1
+
+
+@pytest.mark.parametrize("as_matrix", [False, True])
+def test_mask_and(as_matrix):
+    for mask_dtype in [bool, int]:
+        v1 = Vector(mask_dtype, size=10)
+        v1[3:6] = 0
+        v1[:3] = 10
+        v2 = Vector(mask_dtype, size=10)
+        v2[1::3] = 0
+        v2[::3] = 10
+        if as_matrix:
+            v1 = v1._as_matrix()
+            v2 = v2._as_matrix()
+        masks = [v1.S, v1.V, ~v1.S, ~v1.V, v2.S, v2.V, ~v2.S, ~v2.V]
+        for m1, m2 in itertools.product(masks, masks):
+            expected = Vector(bool, size=10)
+            if as_matrix:
+                expected = expected._as_matrix()
+            expected[...] << True
+            expected = expected.dup(mask=m1).dup(mask=m2)
+            result = (m1 & m2).new()
+            assert result.isequal(expected, check_dtype=True)
+        with pytest.raises(TypeError, match="Invalid mask"):
+            m1 & object()
+        with pytest.raises(TypeError, match="Invalid mask"):
+            object() & m1
+        if v1.dtype == bool:
+            assert isinstance(m1 & v1, Mask)
+            assert isinstance(v1 & m1, Mask)
+        else:
+            with pytest.raises(TypeError, match="Mask must be"):
+                m1 & v1
+            with pytest.raises(TypeError, match="Mask must be"):
+                v1 & m1
