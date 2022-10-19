cb902134d77936317aaf7e4fdc82a210bc149bc3
Erik Welch <erik.n.welch@gmail.com>
2022-10-19 12:57:10 -0500

    Update to SuiteSparse:GraphBLAS 7.3.0 (#291)
    `python-graphblas` is still compatible with 7.2.0, so it was kept as a min version.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     url="https://github.com/python-graphblas/python-graphblas",
     packages=find_packages(),
     python_requires=">=3.8",
-    install_requires=["suitesparse-graphblas >=7.2.0.0, <7.3", "numba", "donfig", "pyyaml"],
+    install_requires=["suitesparse-graphblas >=7.2.0.0, <7.4", "numba", "donfig", "pyyaml"],
     extras_require=extras_require,
     include_package_data=True,
     license="Apache License 2.0",
