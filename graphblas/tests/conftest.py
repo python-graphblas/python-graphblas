944d075dc6ae2d31a6d17433dd7819f09af195de
Erik Welch <erik.n.welch@gmail.com>
2022-11-30 11:26:05 -0600

    Add environment.yml to create dev environment (#340)
    * Add environment.yml to create dev environment
    
    Also, add optional `icecream` fixture :)
    
    * Include `mypy` for type annotations

--- graphblas/tests/conftest.py
+++ graphblas/tests/conftest.py
@@ -84,6 +84,18 @@ def reset_name_counters():
     gb.Scalar._name_counter = itertools.count()
 
 
+@pytest.fixture(scope="session", autouse=True)
+def ic():
+    """Make `ic` available everywhere during testing for easier debugging"""
+    try:
+        import icecream
+    except ImportError:
+        return
+    icecream.install()
+    # icecream.ic.disable()  # This disables icecream; do ic.enable() to re-enable
+    return icecream.ic
+
+
 def autocompute(func):
     @functools.wraps(func)
     def inner(*args, **kwargs):
