e5935fd8f9a74c691727d403c5ab35c55fb07760
Sultan Orazbayev <contact@econpoint.com>
2023-01-13 10:08:17 +0600

    Update io.py

--- graphblas/io.py
+++ graphblas/io.py
@@ -251,7 +251,7 @@ def from_pydata_sparse(A, *, dup_op=None, name=None):
 
     # handle two-dimensional arrays
     import sparse as _sp
-    
+
     if isinstance(A, _sp.GCXS):
         return from_scipy_sparse(A.to_scipy_sparse(), dup_op=dup_op, name=name)
 
