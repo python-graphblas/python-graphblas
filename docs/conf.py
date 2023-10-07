43deb66f5ded8f37e303e9dc1bf525653fe336a5
Erik Welch <erik.n.welch@gmail.com>
2023-10-07 16:40:10 -0500

    Add notebook for creating (and exploring) logos (#506)
    * Add notebook for creating (and exploring) logos
    
    * Install drawsvg when testing notebooks
    
    * Add docs/_static/img/python-graphblas-logo.svg
    
    * update text logos
    
    * Add horizontal and vertical logos (w/ img and text) and update usage

--- docs/conf.py
+++ docs/conf.py
@@ -55,14 +55,16 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "pydata_sphinx_theme"
 
+html_favicon = "_static/img/python-graphblas-logo.svg"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_theme_options = {
     "logo": {
-        "image_light": "_static/img/logo-name-light.svg",
-        "image_dark": "_static/img/logo-name-dark.svg",
+        "image_light": "_static/img/logo-horizontal-light.svg",
+        "image_dark": "_static/img/logo-horizontal-dark.svg",
     },
     "github_url": "https://github.com/python-graphblas/python-graphblas",
 }
--- scripts/check_versions.sh
+++ scripts/check_versions.sh
@@ -4,12 +4,12 @@
 # This may be helpful when updating dependency versions in CI.
 # Tip: add `--json` for more information.
 conda search 'flake8-bugbear[channel=conda-forge]>=23.9.16'
-conda search 'flake8-simplify[channel=conda-forge]>=0.20.0'
+conda search 'flake8-simplify[channel=conda-forge]>=0.21.0'
 conda search 'numpy[channel=conda-forge]>=1.26.0'
 conda search 'pandas[channel=conda-forge]>=2.1.1'
-conda search 'scipy[channel=conda-forge]>=1.11.2'
+conda search 'scipy[channel=conda-forge]>=1.11.3'
 conda search 'networkx[channel=conda-forge]>=3.1'
-conda search 'awkward[channel=conda-forge]>=2.4.3'
+conda search 'awkward[channel=conda-forge]>=2.4.4'
 conda search 'sparse[channel=conda-forge]>=0.14.0'
 conda search 'fast_matrix_market[channel=conda-forge]>=1.7.3'
 conda search 'numba[channel=conda-forge]>=0.57.1'
